fd02dae36649eaa7e4cb817a32046bd52d99909c
Ricky Cook <rcook@odecee.com.au>
2015-03-09 00:31:23 +1100

    Fix apt-fast URL

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-aria2c -d /usr/local/bin "https://github.com/ilikenwf/apt-fast/blob/$APT_FAST_VERSION/apt-fast"
+aria2c -d /usr/local/bin "https://raw.githubusercontent.com/ilikenwf/apt-fast/$APT_FAST_VERSION/apt-fast"
 chmod +x /usr/local/bin/apt-fast
 
 cat > /etc/apt-fast.conf <<-END
