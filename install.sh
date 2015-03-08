a05c1f700291b713252162ab176e78767d72645b
Ricky Cook <rcook@odecee.com.au>
2015-03-09 00:53:42 +1100

    Add apt options

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -e
+[ -z "$NO_APT" -a -z "$NO_APT_UPDATE" ]  && apt-get update
+[ -z "$NO_APT" -a -z "$NO_APT_INSTALL" ] && apt-get install -y aria2 lockfile-progs
+
 aria2c -d /usr/local/bin "https://raw.githubusercontent.com/ilikenwf/apt-fast/$APT_FAST_VERSION/apt-fast"
 chmod +x /usr/local/bin/apt-fast
 
