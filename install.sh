5750510ef6b7f5caa4d9e5c76b0c20b010fb1fd6
Ricky Cook <rcook@odecee.com.au>
2015-03-09 01:01:56 +1100

    Fix config

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 aria2c -d /usr/local/bin "https://raw.githubusercontent.com/ilikenwf/apt-fast/$APT_FAST_VERSION/apt-fast"
 chmod +x /usr/local/bin/apt-fast
 
-cat > /etc/apt-fast.conf <<-END
+cat > /etc/apt-fast.conf <<-"END"
 	DOWNLOADBEFORE=true
-	_DOWNLOADER='aria2c -c -j 5'
+	_DOWNLOADER='aria2c -c -j 20 -i ${DLLIST}'
 END
